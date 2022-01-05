5bcbbe506bd28435260ab696f66352559aee67bc
Ashasriramulu <ashas2401@gmail.com>
2022-01-05 07:44:27 +0000

    Update project IDs and buckets

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,15 +1,50 @@
-variable "region" {
-  default = "us-west1"
-}
-
-variable "location" {
-  default = "us-west1-b"
-}
-
 variable "network_name" {
-  default = "terraform-k8s-demo"
+  type = string
+}
+variable "auto_create_subnetworks" {
+  type = string
 }
 
-variable "project"{
-    default = "digital-maker-333106"
+variable "subnet_name" {
+  type = string
+}
+variable "ip_cidr_range" {
+  type = string
+}
+variable "private_ip_google_access" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type =string
+}
+variable "location" {
+  type = string
+}
+variable "namespace_name"{
+    type = string
+}
+variable "initial_node_count"{
+    type = string
+}
+variable "enable_legacy_abac"{
+    type = string
+}
+
+variable "address_name"{
+    type = string
+}
+variable "service_name"{
+    type = string
+}
+variable "controller_name"{
+    type = string
+}
+
+variable "enable_binary_authorization"{
+    type = string
 }
