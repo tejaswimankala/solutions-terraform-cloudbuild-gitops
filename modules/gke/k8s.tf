ea29fff02bd5dad14916108c2b11c6ff451fe712
Ashasriramulu <50819648+Ashasriramulu@users.noreply.github.com>
2022-01-10 12:21:14 +0530

    Update k8s.tf

--- modules/gke/k8s.tf
+++ modules/gke/k8s.tf
@@ -7,7 +7,7 @@ data "google_container_engine_versions" "default" {
 }
 
 provider "kubernetes" {
-  version = "~> 1.10.0"
+  //version = "~> 1.10.0"
   host    = google_container_cluster.cluster1.endpoint
   token   = data.google_client_config.current.access_token
   client_certificate = base64decode(
